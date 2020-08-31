b1fc3d73ff26b51e625bb078a701f7eee0daaa71
Alireza Rohany <alireza.rohany.77@gmail.com>
2020-08-31 16:02:36 +0430

    initial

--- n_n_t.py
+++ n_n_t.py
@@ -1,8 +1,10 @@
 import tensorflow
-from  tensorflow import keras
+import keras
 import numpy
 import matplotlib.pyplot as plt
 
+# print("neural network is real")
 
+data = keras.datasets.fashion_mnist
 
-print("neural newtwork is real")
+(train_images, train_labels),(test_images, test_labels)=data.load_data()
