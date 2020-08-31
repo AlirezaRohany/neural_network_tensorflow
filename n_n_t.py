59175eff64c4b17817802dcb99b715df51259582
Alireza Rohany <alireza.rohany.77@gmail.com>
2020-08-31 17:22:01 +0430

    loading data and normalize pixel values between 0 and 1

--- n_n_t.py
+++ n_n_t.py
@@ -7,4 +7,17 @@ import matplotlib.pyplot as plt
 
 data = keras.datasets.fashion_mnist
 
-(train_images, train_labels),(test_images, test_labels)=data.load_data()
+(train_images, train_labels), (test_images, test_labels) = data.load_data()
+# print(train_labels[6])
+
+class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
+               'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
+# print(class_names[train_labels[6]])
+
+train_images = train_images / 255.0
+test_images = test_images / 255.0
+
+# print(train_images[100])
+
+plt.imshow(train_images[100], cmap=plt.cm.binary)
+plt.show()
