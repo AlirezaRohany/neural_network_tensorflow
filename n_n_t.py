aa3d43f13934de6bdba0799919d8b0bb66542b19
Alireza Rohany <alireza.rohany.77@gmail.com>
2020-09-01 23:01:40 +0430

    started creating a model

--- n_n_t.py
+++ n_n_t.py
@@ -19,5 +19,12 @@ test_images = test_images / 255.0
 
 # print(train_images[100])
 
-plt.imshow(train_images[100], cmap=plt.cm.binary)
-plt.show()
+# plt.imshow(train_images[100], cmap=plt.cm.binary)
+# plt.show()
+
+# creating a model
+model = keras.Sequential([
+    keras.layers.Flatten(input_shape=(28, 28)),  # input layer, flatten our pixels (flatten input layer)
+    keras.layers.Dense(128, activation="relu"),  # hidden layer, with 'rectified linear unit' activation function
+    keras.layers.Dense(10, activation="softmax")  # output layer
+])
