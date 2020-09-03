c4f500c2afab0fa460824c3506c62898a208b023
Alireza Rohany <alireza.rohany.77@gmail.com>
2020-09-03 20:42:42 +0430

    used model to make predictions and compare predictions with real values

--- n_n_t.py
+++ n_n_t.py
@@ -30,12 +30,30 @@ model = keras.Sequential([
 ])
 
 # setting up some parameters for our model
-model.compile(optimizer="adam", loss="sparse_categorical_crossentropy", metrics=["accuracy"])
 # adam is typical optimizer, loss function can vary based on our application
+model.compile(optimizer="adam", loss="sparse_categorical_crossentropy", metrics=["accuracy"])
 
 # training our model
-model.fit(train_images, train_labels, epochs=10)  # epochs is how many times it's gonna see the  same image
+model.fit(train_images, train_labels, epochs=5)  # epochs is how many times it's gonna see the  same image
 
 # testing our model
 test_loss, test_acc = model.evaluate(test_images, test_labels)
 print("\n", "Test accuracy:", test_acc)
+
+# using the model to make predictions
+predictions = model.predict(test_images)
+
+for i in range(10):
+    plt.grid(False)
+    plt.imshow(test_images[i], cmap=plt.cm.binary)
+    plt.xlabel("Real class: " + class_names[test_labels[i]])
+    plt.title("Predicted class: " + class_names[numpy.argmax(predictions[i])])
+    plt.show()
+
+## testing a single item
+# test_item=72
+# predictions=model.predict(numpy.array([test_images[test_item]]))
+# plt.imshow(test_images[test_item], cmap=plt.cm.binary)
+# plt.xlabel("Real class: " + class_names[test_labels[test_item]])
+# plt.title("Predicted class: " + class_names[numpy.argmax(predictions)])
+# plt.show()
